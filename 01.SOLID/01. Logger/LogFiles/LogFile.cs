118ff1d208286615d540b4bddf1fc63be52ca679
Borislav Kunchev <gearboxxbg@gmail.com>
2019-02-17 19:50:09 +0200

    Exp 1 Added
    Solid exp added / finished

---
+++ 01.SOLID/01. Logger/LogFiles/LogFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+public class LogFile : ILogFile
+{
+	private string path = "../../../log.txt";
+
+	public LogFile()
+	{
+		FileStream fs = new FileStream(path, FileMode.Create);
+		fs.Close();
+	}
+
+	public int Size
+	{
+		get
+		{
+			using (StreamReader stream = new StreamReader(path))
+			{
+				return stream.ReadToEnd().ToCharArray()
+					.Where(Char.IsLetter)
+					.Sum(c => c);
+			}
+		}
+	}
+
+	public void Write(string content)
+	{
+		using (StreamWriter stream = new StreamWriter(path, true))
+		{
+			stream.WriteLine(content);
+		}
+	}
+}
---
+++ 01.SOLID/01. Logger/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int countOfAppenders = int.Parse(Console.ReadLine());
+		Controller controller = new Controller();
+		controller.Act(countOfAppenders);
+	}
+}
